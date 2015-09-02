0cabc2763155a3d6db1dec580a5afabe671fe70f
Buddha Prakash <bprakashjsr@gmail.com>
2015-09-02 19:58:54 +0530

    Add Baseline Estimator

--- asag/baseline/baseline_predictor.py
+++ asag/baseline/baseline_predictor.py
@@ -4,6 +4,9 @@ from preprocess.parser import get_data
 from preprocess.preprocess import correct_student_answers
 from scipy import spatial
 from similarity_measures import get_cosine_similarity
+from sklearn import tree
+from sklearn.ensemble import RandomForestClassifier
+
 def get_all_features_question(question, student_answer):
 	cosine_sim = get_cosine_similarity(question,student_answer)
 	return [cosine_sim]
@@ -16,27 +19,55 @@ def get_all_features_ref(reference_answers, student_answer):
 			max_cosine_sim = cosine_sim
 	return [max_cosine_sim]
 
-def get_single_train(data_point):
+def get_single_data(data_point):
 	question = data_point[0]
 	reference_answers = [ans for ans, _ in data_point[1]]
-	train_data = []
+	feature_data = []
 	for student_answer, label in data_point[2]:
-		train_row = get_all_features_question(question, student_answer)
-		train_row = train_row + get_all_features_ref(reference_answers, student_answer)
-		train_row = train_row + [1 if label=="correct" else 0]
-		train_data.append(train_row)
-	return train_data
+		feature_row = get_all_features_question(question, student_answer)
+		feature_row = feature_row + get_all_features_ref(reference_answers, student_answer)
+		feature_row = feature_row + [1 if label=="correct" else 0]
+		feature_data.append(feature_row)
+	return feature_data
 
-def get_training_data(data):
-	train_data = []
+def get_feature_data(dir_path):
+	data = get_data(dir_path)
+	data = correct_student_answers(data)
+	feature_data = []
 	for data_point in data:
-		train_data += get_single_train(data_point)
+		feature_data += get_single_data(data_point)
+	return feature_data
+
+def split_labels(data):
+	X = []
+	y = []
+	for row in data:
+		X.append(row[:-1])
+		y.append(row[-1])
+	return X, y
+
+def compute_accuracy(pred_Y,test_Y):
+	correct_count = 0
+	for i in range(len(pred_Y)):
+		if pred_Y[i] == test_Y[i] :
+			correct_count += 1
+	return (float(correct_count))/len(pred_Y)
+
+def fit_predict(data_train, data_test):
+	train_X, train_Y = split_labels(data_train)
+	test_X, test_Y = split_labels(data_test)
+	clf = RandomForestClassifier(n_estimators=10)
+	clf = clf.fit(train_X, train_Y)
+	pred_Y = clf.predict(test_X)
+	accuracy = compute_accuracy(pred_Y, test_Y)
+	print accuracy
 
 def main():
 	dir_path = "../data/semeval2013-Task7-2and3way/training/2way/beetle"
-	data = get_data(dir_path)
-	data = correct_student_answers(data)
-	train_data = get_training_data(data)
+	data_train = get_feature_data(dir_path)
+	dir_path = "../data/semeval2013-Task7-2and3way/test/2way/beetle/test-unseen-answers"
+	data_test = get_feature_data(dir_path)
+	fit_predict(data_train, data_test)
 
 if __name__ == '__main__':
 	main()
--- asag/baseline/similarity_measures.py
+++ asag/baseline/similarity_measures.py
@@ -5,10 +5,14 @@ from sklearn.feature_extraction.text import CountVectorizer
 def get_cosine_similarity(string1, string2):
 	count_vect = CountVectorizer()
 	X_train_counts = count_vect.fit_transform([string1,string2])
-	X_train_counts = X_train_counts.todense()
-	return 1 - spatial.distance.cosine(X_train_counts[0],X_train_counts[1])
+	X_train_counts = X_train_counts.todense().tolist()
+	if (sum(map(lambda x:x*x,X_train_counts[0])) == 0) or (sum(map(lambda x:x*x,X_train_counts[1])) == 0):
+		cosine_sim = 0.0
+	else:
+		cosine_sim = 1 - spatial.distance.cosine(X_train_counts[0],X_train_counts[1])
+	return cosine_sim
 
-def computeLeskSimilarity():
+def get_lesk_similarity(string1, string2):
 	return
 
 def main():

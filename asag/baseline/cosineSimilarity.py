ecebf2ffe1632adf377ee26d6bfcb7b7d232ff37
Buddha Prakash <bprakashjsr@gmail.com>
2015-09-02 07:18:29 +0530

    Add larger SemEval dataset

--- asag/baseline/cosineSimilarity.py
+++ asag/baseline/cosineSimilarity.py
@@ -1,1 +1,11 @@
-san
+import parser
+import spell
+import preprocess
+
+def main():
+	dir_path = "../data/2-3way_SemEvalData/2way/beetle"
+	data = parser.get_data(dir_path)
+	data = preprocess.correct_student_answers(data)
+
+if __name__ == '__main__':
+	main()
